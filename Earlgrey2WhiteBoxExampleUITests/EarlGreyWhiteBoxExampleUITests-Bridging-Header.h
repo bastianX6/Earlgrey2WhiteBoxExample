730b5ba0204923819d05fc095e95d7e90b07c771
Bastian Veliz <bastianx6@gmail.com>
2020-04-18 18:46:57 -0400

    Integrate EarlGrey2 with swift support

---
+++ Earlgrey2WhiteBoxExampleUITests/EarlGreyWhiteBoxExampleUITests-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  EarlGreyWhiteBoxExampleUITests-Bridging-Header.h
+//  Earlgrey2WhiteBoxExample
+//
+//  Created by Bastián Véliz Vega on 18-04-20.
+//  Copyright © 2020 Bastián Véliz Vega. All rights reserved.
+//
+
+#import "AppFramework/Action/GREYAction.h"
+#import "AppFramework/Action/GREYActionBlock.h"
+#import "AppFramework/Action/GREYActions.h"
+#import "CommonLib/DistantObject/GREYHostApplicationDistantObject.h"
+#import "CommonLib/Matcher/GREYElementMatcherBlock.h"
+#import "CommonLib/Matcher/GREYMatcher.h"
+#import "TestLib/AlertHandling/XCTestCase+GREYSystemAlertHandler.h"
+#import "TestLib/EarlGreyImpl/EarlGrey.h"
