93d0dbe8db13eb8d455a8e8d9fd65977a32f47f5
Bastian Veliz <bastianx6@gmail.com>
2020-04-18 17:52:36 -0400

    Add initial view structure

--- Earlgrey2WhiteBoxExampleUITests/Earlgrey2WhiteBoxExampleUITests.swift
+++ Earlgrey2WhiteBoxExampleUITests/Earlgrey2WhiteBoxExampleUITests.swift
@@ -9,7 +9,6 @@
 import XCTest
 
 class Earlgrey2WhiteBoxExampleUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -30,14 +29,5 @@ class Earlgrey2WhiteBoxExampleUITests: XCTestCase {
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
