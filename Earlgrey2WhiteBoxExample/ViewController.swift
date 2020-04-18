f8d72fb4bda43de46d602a3121cc275841bde6d5
Bastian Veliz <bastianx6@gmail.com>
2020-04-18 17:43:34 -0400

    Initial Commit

---
+++ Earlgrey2WhiteBoxExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Earlgrey2WhiteBoxExample
+//
+//  Created by Bastián Véliz Vega on 18-04-20.
+//  Copyright © 2020 Bastián Véliz Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
